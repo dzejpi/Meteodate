8caefc61143cb838f643c7619129f1b2d98ed77a
dzejpi <dzejjpii@gmail.com>
2022-04-03 10:09:27 +0200

    Fixed the dialog issue I spent like 3 hours on. Two quotation marks!!! I need a drink and it’s only 10 am.

--- scripts/DialogManager.gd
+++ scripts/DialogManager.gd
@@ -13,14 +13,14 @@ func _process(delta):
 
 
 func get_dialog(dialog_number):
-	var dialog_text = []
+	var dialog_text = [""]
 	
 	match(dialog_number):
 		1:
-			dialog_text = ["First dialog."]
+			dialog_text = ["RIRrTws4W2E4p22JeBS6ISvil8mH7ExtXPCgBPMARopBLHLy80xnDzJMGj6NRDmctX89QRHuXNoTQ6KmsB82EHoioaXd9FUdEXsNgN88YxYi53LTX0plXMbR57", "8mH7ExtXPCgBPMARopBLHLy80xnDzJMGj6NRDmctX89QRHuXNoTQ6KmsB82EHoioaXd9FUd"]
 		2:
-			dialog_text = ["Second dialog"]
+			dialog_text = ["Second dialog", "Second part."]
 		3:
-			dialog_text = ["Third dialog"]
+			dialog_text = ["Third dialog.", "First part."]
 
 	return dialog_text
